1bc513af8b839402f05948f174e7f755ce5f7ea2
iovann <iovannatcho@gmail.com>
2024-05-24 17:44:00 +0100

    bdd

--- college_management_system/settings.py
+++ college_management_system/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     # Third Part Middleware
     'whitenoise.middleware.WhiteNoiseMiddleware',
 
